06b02cfd19327cf574003d51c54177279d992242
Simon Neidig <simon1234@online.de>
2020-10-27 19:22:25 +0100

    Implement callback-function for route "/contact"

--- api/v1/routes/contact/get-contact.php
+++ api/v1/routes/contact/get-contact.php
@@ -1,8 +1,33 @@
 <?php
 
-function contacts_get() {
-    $resp = new WP_REST_Response('Contacts');
-    $resp->set_status(200);
-    $resp->header('Content-type', 'application/json');
-    return $resp;
+/**
+ * This method handles GET-requests to the route "/contact". It returns all contact-persons as they are in the database.
+ * 
+ * @return  WP_REST_Response
+ *      - 200   :   Everything went well, an array of all contact-persons will be returned.
+ *      - 500   :   An Exception was thrown, the exception-message will be returned in a json-format.
+ */
+function contacts_get()
+{
+    try {
+        global $wpdb;
+        $body = array();
+        $persons = $wpdb->get_results("SELECT * FROM " . $wpdb->prefix . "mvm1914_contact");
+        foreach ($persons as $person) {
+            $new_person["name"] = $person->name;
+            $new_person["person"] = $person->person;
+            $new_person["mail"] = $person->mail;
+            array_push($body, $new_person);
+        }
+
+        $resp = new WP_REST_Response($body);
+        $resp->set_status(200);
+        $resp->header('Content-type', 'application/json');
+        return $resp;
+    } catch (Exception $e) {
+        $resp = new WP_REST_Response(generate_error_body($e));
+        $resp->set_status(500);
+        $resp->header('Content-type', 'application/json');
+        return $resp;
+    }
 }
--- api/v1/routes/group/get-group.php
+++ api/v1/routes/group/get-group.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /**
  * This method handles GET-requests to the route "/group". It returns all groups as they are in the database.
  * 
@@ -10,32 +9,39 @@
  */
 function groups_get()
 {
-    global $wpdb;
-    $body = array();
-    $groups = $wpdb->get_results("SELECT * FROM " . $wpdb->prefix . "mvm1914_group");
-    foreach ($groups as $group) {
-        $new_group["name"] = $group->name;
-        $new_group["managment"] = $group->management;
-        $new_group["time"] = $group->time;
+    try {
+        global $wpdb;
+        $body = array();
+        $groups = $wpdb->get_results("SELECT * FROM " . $wpdb->prefix . "mvm1914_group");
+        foreach ($groups as $group) {
+            $new_group["name"] = $group->name;
+            $new_group["managment"] = $group->management;
+            $new_group["time"] = $group->time;
 
-        $location = $wpdb->get_results("SELECT * FROM " . $wpdb->prefix . "mvm1914_location WHERE id=\"" . $group->location_id . "\";");
-        if (count($location) > 0) {
-            $new_location["name"] = $location[0]->name;
-            $new_location["street"] = $location[0]->street;
-            $new_location["city"] = $location[0]->city;
-            $new_location["state"] = $location[0]->state;
-            $new_location["country"] = $location[0]->country;
-            $new_location["plz"] = $location[0]->plz;
-            $new_location["latitude"] = $location[0]->latitude;
-            $new_location["longitude"] = $location[0]->longitude;
-            $new_group["location"] = $new_location;
+            $location = $wpdb->get_results("SELECT * FROM " . $wpdb->prefix . "mvm1914_location WHERE id=\"" . $group->location_id . "\";");
+            if (count($location) > 0) {
+                $new_location["name"] = $location[0]->name;
+                $new_location["street"] = $location[0]->street;
+                $new_location["city"] = $location[0]->city;
+                $new_location["state"] = $location[0]->state;
+                $new_location["country"] = $location[0]->country;
+                $new_location["plz"] = $location[0]->plz;
+                $new_location["latitude"] = $location[0]->latitude;
+                $new_location["longitude"] = $location[0]->longitude;
+                $new_group["location"] = $new_location;
+            }
+
+            array_push($body, $new_group);
         }
 
-        array_push($body, $new_group);
+        $resp = new WP_REST_Response($body);
+        $resp->set_status(200);
+        $resp->header('Content-type', 'application/json');
+        return $resp;
+    } catch (Exception $e) {
+        $resp = new WP_REST_Response(generate_error_body($e));
+        $resp->set_status(500);
+        $resp->header('Content-type', 'application/json');
+        return $resp;
     }
-
-    $resp = new WP_REST_Response($body);
-    $resp->set_status(200);
-    $resp->header('Content-type', 'application/json');
-    return $resp;
 }
